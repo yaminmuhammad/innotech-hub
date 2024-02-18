71a68a7790b628c9f1c202a2ed1a508593269dae
yaminmuhammad <1910631170108@student.unsika.ac.id>
2024-02-18 18:54:07 +0700

    fix: navbar mobile

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
 
 	return (
 		<>
-			<div className="bg-white dark:bg-black dark:text-white text-black overflow-x-hidden">
+			<div className="bg-white dark:bg-black dark:text-white text-black overflow-x-hidden body">
 				<Navbar />
 				<Hero />
 				<BrandsLogo />
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ export default {
 	theme: {
 		extend: {
 			fontFamily: {
-				inter: ["Inter", "sans-serif"],
+				sans: ["Helvetica", "Arial", "sans-serif"],
 			},
 			colors: {
 				primary: "#6153CD",
